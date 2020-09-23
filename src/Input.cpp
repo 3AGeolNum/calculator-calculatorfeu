4f4014e6658cff79b4bf8e1b9b768bad199599df
KilyannRichard <kilyann.richard2@etu.univ-lorraine.fr>
2020-09-23 16:45:48 +0200

    Removing spaces function added

--- src/Input.cpp
+++ src/Input.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 std::string getUserInput(){
 
     std::string input_from_user = "";
-    std::cout << "Please enter your calculation (only with +,-,*,/ and figures)" << std::endl;
+    std::cout << "Please enter your calculation (only with '+','-','*','/', ',', '.' and figures)" << std::endl;
     std::cin >> input_from_user;
     std::cout << "You chose the calculation " << input_from_user << std::endl;
     return input_from_user;
+
+}
+
+void remove_space(std::string &string_with_spaces){
+
+    std::string str_without_spaces;
+    int size_of_string = string_with_spaces.size();
+    const char* space = " ";
+    for (int i=0; i<size_of_string; ++i){
+
+        if (string_with_spaces[i] != *space){
+            str_without_spaces.push_back(string_with_spaces[i]);
+        }
+
+    }
+
+    string_with_spaces = str_without_spaces;
 
 }
 

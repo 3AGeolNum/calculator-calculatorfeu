958a7a4cdcd9c062d4b8fdabadb1ab34d2e1f39a
Laure98 <laure.su6@etu.univ-lorraine.fr>
2020-09-29 16:23:47 +0200

    updated the file

--- test/test_Process.cpp
+++ test/test_Process.cpp
@@ -1,27 +1,17 @@
-#include <Process.cpp>
+#include <Process.h>
 
 int test_formation(){
 	std::string input1 = "3+5"; //testing the formation of an additive Tree
-	Tree* tree1 = process(input1);
+	Tree* tree1 = doProcess(input1);
 
 	std::string input2 = "3-5"; //testing the formation of a substractive Tree
-        Tree* tree2 = process(input2);
+        Tree* tree2 = doProcess(input2);
         
 	std::string input3 = "3*5"; //testing the formation of a multiplicative Tree
-        Tree* tree3 = process(input3);
+        Tree* tree3 = doProcess(input3);
         
 	std::string input4 = "3/5"; //testing the formation of a divisive Tree
-        Tree* tree4 = process(input4);
+        Tree* tree4 = doProcess(input4);
 	
-	return 1;
-}
-
-int test_priorities(){
-	std::string input = "3+5*2-9-1/5";
-	Tree* tree = process(input);
-        while( tree.lhs_ != nullptr ){
-		assert( tree.rhs_ != nullptr );
-		
-		}
-	return 1;	
+	return 0;
 }

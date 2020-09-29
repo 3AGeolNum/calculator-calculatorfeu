62c22544f1a00c1a8f40729d16efcd52f040b19d
auto-reformat <auto-reformat@auto.reformat>
2020-09-29 15:08:21 +0000

    [Auto] Reformat

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -2,46 +2,50 @@
 #include <iostream>
 
 <<<<<<< HEAD
-Tree::Tree(std::string val) : val_(val), lhs_(nullptr), rhs_(nullptr){
+Tree::Tree( std::string val ) : val_( val ), lhs_( nullptr ), rhs_( nullptr ) {}
+
+Tree::Tree( std::string val, Tree* lhs, Tree* rhs ) : val_( val )
+{
+    rhs_ = new Tree( *rhs );
+    lhs_ = new Tree( *lhs );
 }
 
-Tree::Tree(std::string val, Tree* lhs, Tree* rhs) : val_(val){
-	rhs_ = new Tree(*rhs);
-	lhs_ = new Tree(*lhs);
+Tree::Tree( const Tree& tree )
+{
+    val_ = tree.val_;
+    if( tree.rhs_ == nullptr )
+    {
+        rhs_ = nullptr;
+    }
+    else
+    {
+        rhs_ = new Tree( *( tree.rhs_ ) );
+    }
+    if( tree.lhs_ == nullptr )
+    {
+        lhs_ = nullptr;
+    }
+    else
+    {
+        lhs_ = new Tree( *( tree.lhs_ ) );
+    }
 }
 
-Tree::Tree(const Tree& tree){
-	val_ = tree.val_;
-	if (tree.rhs_ == nullptr){
-		rhs_ = nullptr;	
-	}
-	else{
-		rhs_ = new Tree(*(tree.rhs_));
-	}
-	if (tree.lhs_ == nullptr){
-		lhs_ = nullptr;	
-	}
-	else{
-		lhs_ = new Tree(*(tree.lhs_));
-	}
+Tree::~Tree()
+{
+    std::cout << "DESTRUCTOR " << this << std::endl;
 
-}
+    if( lhs_ != nullptr )
+    {
+        std::cout << "LHS NOT NULL for " << this << std::endl;
 
-Tree::~Tree(){
-
-	std::cout << "DESTRUCTOR " << this << std::endl;
-
-	if (lhs_ != nullptr){
-
-		std::cout << "LHS NOT NULL for " << this << std::endl;
-			
-		delete lhs_;
-	}
-	if (rhs_ != nullptr){	
-
-		std::cout << "RHS NOT NULL for " << this << std::endl;
-		delete rhs_;
-	}
+        delete lhs_;
+    }
+    if( rhs_ != nullptr )
+    {
+        std::cout << "RHS NOT NULL for " << this << std::endl;
+        delete rhs_;
+    }
 }
 
 double Tree::eval()
--- test/test_Tree.cpp
+++ test/test_Tree.cpp
@@ -4,27 +4,31 @@
 
 int main()
 {
-    Tree tree1 = Tree("3");
-    
-    Tree tree2("6");
-    Tree* ptr_tree1 = &tree1;
-	Tree* ptr_tree2 = &tree2;
+    Tree tree1 = Tree( "3" );
 
-	Tree tree3("+", ptr_tree1, ptr_tree2);
-	Tree tree4("-", ptr_tree1, ptr_tree2);
-	Tree tree5("*", ptr_tree1, ptr_tree2);
-	Tree tree6("/", ptr_tree1, ptr_tree2);
-	if (tree3.eval() != 9) {
-		return 1;
-	}
-	else if( tree4.eval() != -3){
-		return 1;
-	}
-	else if (tree5.eval() != 18) {
-		return 1;
-	}
-	else if (tree6.eval() != 0.5) {
-		return 1;
-	}
+    Tree tree2( "6" );
+    Tree* ptr_tree1 = &tree1;
+    Tree* ptr_tree2 = &tree2;
+
+    Tree tree3( "+", ptr_tree1, ptr_tree2 );
+    Tree tree4( "-", ptr_tree1, ptr_tree2 );
+    Tree tree5( "*", ptr_tree1, ptr_tree2 );
+    Tree tree6( "/", ptr_tree1, ptr_tree2 );
+    if( tree3.eval() != 9 )
+    {
+        return 1;
+    }
+    else if( tree4.eval() != -3 )
+    {
+        return 1;
+    }
+    else if( tree5.eval() != 18 )
+    {
+        return 1;
+    }
+    else if( tree6.eval() != 0.5 )
+    {
+        return 1;
+    }
     return 0;
 }

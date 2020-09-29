cd55eb7796c35f4efb9fc0560442d17d0c28da46
Laure Su <su12u@etu.univ-lorraine.fr>
2020-09-29 16:36:11 +0200

    modif test

--- test/test_Process.cpp
+++ test/test_Process.cpp
@@ -1,17 +1,11 @@
 #include <Process.h>
 
 int test_formation(){
-	std::string input1 = "3+5"; //testing the formation of an additive Tree
-	Tree* tree1 = doProcess(input1);
+	std::string input1 = "3+5/15*9-10";
+	Tree* ptr_tree1 = doProcess(input1);
+	if (ptr_tree1->eval() != -4) {
+		return 1;
+	}
 
-	std::string input2 = "3-5"; //testing the formation of a substractive Tree
-        Tree* tree2 = doProcess(input2);
-        
-	std::string input3 = "3*5"; //testing the formation of a multiplicative Tree
-        Tree* tree3 = doProcess(input3);
-        
-	std::string input4 = "3/5"; //testing the formation of a divisive Tree
-        Tree* tree4 = doProcess(input4);
-	
 	return 0;
 }

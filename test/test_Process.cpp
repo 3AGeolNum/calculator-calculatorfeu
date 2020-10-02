233e9ce000bddbd92f58e65d195330c9b9454d54
Laure98 <laure.su6@etu.univ-lorraine.fr>
2020-10-02 12:08:10 +0200

    forgot tho change the number for the test2

--- test/test_Process.cpp
+++ test/test_Process.cpp
@@ -3,15 +3,15 @@
 int main()
 {
     std::string input1 = "3+5/15*9-10";
-    Tree* ptr_tree1 = doProcess( input1 );
+    Tree* ptr_tree1 = do_process( input1 );
     if( ptr_tree1->eval() != -4 )
     {
         return 1;
     }
 
     std::string input2 = "_1+5*9/3";
-    Tree* ptr_tree2 = doProcess( input2 );
-    if( ptr_tree1->eval() != 14 )
+    Tree* ptr_tree2 = do_process( input2 );
+    if( ptr_tree2->eval() != 14 )
     {
         return 1;
     }

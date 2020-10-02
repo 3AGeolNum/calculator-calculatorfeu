172f782eb67b4c3694b0fb06ee0873937d85967a
Laure98 <laure.su6@etu.univ-lorraine.fr>
2020-10-02 12:17:57 +0200

    changed the name on the main

--- main/Calculator.cpp
+++ main/Calculator.cpp
@@ -1,7 +1,20 @@
+#include <Input.cpp>
+#include <Process.h>
 #include <iostream>
 
-int main( int argc, char** arv )
+int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    std::string expression;
+
+    expression = getUserInput( expression );
+    while( !checkInput( expression ) )
+    {
+        expression = getUserInput( expression );
+    }
+
+    Tree* ptr_tree = do_process( expression );
+    Tree tree = &ptr_tree;
+    std::cout << "Result: " << tree.eval() << std::endl;
+
     return 0;
 }

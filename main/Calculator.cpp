9c629e885634ea0dde784f0f7b575a9e14af59ba
auto-reformat <auto-reformat@auto.reformat>
2020-10-02 10:32:36 +0000

    [Auto] Reformat

--- main/Calculator.cpp
+++ main/Calculator.cpp
@@ -5,9 +5,10 @@ int main()
 {
     std::string expression;
 
-    do{
-		expression = getUserInput();
-	} while( !check_and_format_Input( expression ) );
+    do
+    {
+        expression = getUserInput();
+    } while( !check_and_format_Input( expression ) );
 
     Tree* ptr_tree = do_process( expression );
     std::cout << "Result: " << ptr_tree->eval() << std::endl;

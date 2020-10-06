1271453f31379cf3a20822a586f7d98bcf18d6b8
Charlie Garayt <garayt1u@etu.univ-lorraine.fr>
2020-10-06 11:41:47 +0200

    Very useful feature

--- main/Calculator.cpp
+++ main/Calculator.cpp
@@ -2,8 +2,36 @@
 #include <Process.h>
 #include <iostream>
 
+#ifdef _WIN32
+#include <Lmcons.h>
+#include <windows.h>
+#endif
+
 int main()
 {
+#ifdef _WIN32
+
+    // USEFUL EASTER EGG
+
+    char username[UNLEN + 1];
+    DWORD username_len = UNLEN + 1;
+    GetUserName( username, &username_len );
+    std::string user = std::string( username );
+
+    if( !user.rfind( "caumon", 0 ) )
+    {
+        std::cout << "[CRITICAL ERROR]" << std::endl;
+        std::cout << "SORRY, YOU ARE NOT ALLOWED TO RUN THIS PROGRAM."
+                  << std::endl;
+        std::cout << "After a team survey, the statistical data have shown "
+                     "that the likelihood of you breaking me is too high for "
+                     "me to dare take up the risk of allowing you that."
+                  << std::endl;
+        std::cout << "Sincerely yours, Fire Calculator." << std::endl;
+        std::cin.get();
+    }
+#endif
+
     std::string expression;
 
     do
